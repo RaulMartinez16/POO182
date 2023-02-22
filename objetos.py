6ba27942370e9b9c11ec21e87c3166b21bcf8bbc
RaulMartinez16 <125922654+RaulMartinez16@users.noreply.github.com>
2023-02-22 14:17:33 -0600

    Objetos y mas

---
+++ objetos.py
@@ -0,0 +1,9 @@
+from personaje import*
+
+#creamos una instancia Heroe de la clase personaje
+Heroe= Personaje()
+
+#usamos los atributos
+print("El personaje se llama"+ Heroe.nombre)
+print("pertenece a la especie"+ Heroe.especie)
+print("y una atura de"+ Heroe.altura
--- personaje.py
+++ personaje.py
@@ -4,3 +4,18 @@ class Personaje:
     especie="humanos"
     nombre="Ares"
     altura=1.85
+
+    #metodos
+    def correr(self,status):
+        if(status):
+            print("El personaje "+ self.nombre+" esta corriendo")
+        else 
+            print("El personaje "+ self.nombre+" se detuvo")
+                  
+def lanzarGranadas(self): 
+    print("El personaje "+ self.nombre+" lanzo granada)
+          
+def recargarArma(self,municiones):
+cargador=10
+cargador= cargador+municiones
+ print("El arma recargada tiene "+ cargador "balas" )
