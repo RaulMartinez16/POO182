d220aa893984350817def4fbdade74477699647f
RaulMartinez16 <125922654+RaulMartinez16@users.noreply.github.com>
2023-02-22 15:34:11 -0600

    Ejemplificador

--- personaje.py
+++ personaje.py
@@ -1,21 +1,22 @@
 class Personaje:
-
-    #atributos
-    especie="humanos"
-    nombre="Ares"
-    altura=1.85
-
+    
+    def _init_(self,esp,nom,alt):
+        #atributos
+        self.especie = esp
+        self.nombre = nom
+        self.altura = alt
+    
     #metodos
     def correr(self,status):
-        if(status):
-            print("El personaje "+ self.nombre+" esta corriendo")
-        else 
-            print("El personaje "+ self.nombre+" se detuvo")
-                  
-def lanzarGranadas(self): 
-    print("El personaje "+ self.nombre+" lanzo granada)
-          
-def recargarArma(self,municiones):
-cargador=10
-cargador= cargador+municiones
- print("El arma recargada tiene "+ cargador "balas" )
+        if (status):
+            print("El personaje " + self.nombre + " esta corriendo")
+        else: 
+            print("El personaje " + self.nombre + " se detuvo")
+            
+    def lanzarGranadas(self):
+        print ("El personaje " + self.nombre + " lanzo granada")
+    
+    def recargarArma(self,municiones):
+        cargador = 10
+        cargador = cargador + municiones
+        print ("El arma recargada tiene " + str(cargador)+" balas")
